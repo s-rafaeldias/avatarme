28fa38f29bb3bab00f17065099d031f90ad79731
Rafael Dias <rafa197795@gmail.com>
2019-05-18 00:32:11 -0300

    Generate MD5 from a []byte

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func getMD5(data []byte) [md5.Size]byte {
+	return md5.Sum(data)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	text := getMD5([]byte("teste"))
+	fmt.Printf("%x\n", text)
 }
