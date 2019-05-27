656386adc7d6418874f18b7d8efec9be86c1c12a
Rafael Dias <rafa197795@gmail.com>
2019-05-26 21:43:41 -0300

    Fill corner panes in blue

--- main.go
+++ main.go
@@ -11,16 +11,39 @@ func getMD5(data []byte) [md5.Size]byte {
 	return md5.Sum(data)
 }
 
+func drawPane(dc *gg.Context, x, y, r, g, b float64) {
+	dc.Push()
+	dc.SetRGB(r, g, b)
+	dc.DrawRectangle(x, y, 200, 200)
+	dc.Fill()
+	dc.Pop()
+}
+
+func drawCorner(dc *gg.Context) {
+	// Draw top left
+	drawPane(dc, 0, 0, 0, 0, 1)
+	// Draw top right
+	drawPane(dc, 400, 0, 0, 0, 1)
+	// Draw bottom left
+	drawPane(dc, 0, 400, 0, 0, 1)
+	// Draw bottom right
+	drawPane(dc, 400, 400, 0, 0, 1)
+
+}
+
 func main() {
 	// Generate MD5
 	text := getMD5([]byte("teste"))
 	fmt.Printf("%x\n", text)
 
-	filename := "teste.png"
-	const Size = 500
+	// Constants
+	const Size = 600
+
+	// identicon is 600x600
 	dc := gg.NewContext(Size, Size)
-	dc.DrawCircle(250., 250., 50.)
-	dc.SetRGB(1, 1, 1)
-	dc.Fill()
-	dc.SavePNG(filename)
+
+	drawCorner(dc)
+
+	dc.SaveJPG("teste.jpg", 100)
+
 }
